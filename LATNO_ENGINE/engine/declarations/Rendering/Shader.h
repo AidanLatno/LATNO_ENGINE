13169094c14efbd6415b9dc9f07e21901a06d58a
Deep-Singh14 <khushdip14@gmail.com>
2023-04-01 19:14:02 -0700

    Added Sprite render but some errors

--- LATNO_ENGINE/engine/declarations/Rendering/Shader.h
+++ LATNO_ENGINE/engine/declarations/Rendering/Shader.h
@@ -26,12 +26,16 @@ public:
 
 	// set uniforms
 	void SetUniform1f(const std::string& name, float value); // takes in single float
-	void SetUniform1i(const std::string& name, int value); // takes in single int
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform3f(const std::string& name, float x, float y, float z);
+	void SetUniform3f(const std::string& name, const glm::vec3& value);
+	// takes in single int
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3); // takes in 4 floats (vec4)
 	void SetUniformMat4f(const std::string& name, const glm::mat4 matrix); // takes in mat4(glm library) for matrix projection
 private:
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	Shader& Use();
 	ShaderProgramSource ParseShader(const std::string& filepath);
 
 	int GetUniformLocation(const std::string& name);
