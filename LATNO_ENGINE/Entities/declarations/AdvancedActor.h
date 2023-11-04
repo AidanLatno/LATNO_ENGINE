801481bd83b7c89d85897155645b1e25cf271478
AidanLatno <aidlatno@outlook.com>
2023-11-04 11:25:50 -0700

    Documentation for AdvancedActor Finished

--- LATNO_ENGINE/Entities/declarations/AdvancedActor.h
+++ LATNO_ENGINE/Entities/declarations/AdvancedActor.h
@@ -14,13 +14,18 @@ namespace Latno {
 	*		- Pointer to a scene
 	*		- Meant to point to the current scene that is being rendered
 	* 
-	*	MoveForward(Scene &Scene, float deltaTime)
+	*	MoveForward(Scene &Scene)
 	*		- Moves self in the direction this actor is facing, accounting for the speed attribute
 	*		- Does not allow actor to travel outside of the passed in scene
 	*	MoveForward(int SizeX,int SizeY)
+	*		- Moves self in the direction this actor is facing, accounting for the speed attribute
+	*		- Does not allow actor to travel outside of the given sizes
 	*	MoveForward()
+	*		- Moves self in the direction this actor is facing, accounting for the speed attribute
 	*	TurnRight(float deg)
+	*		- Turns the actors direction to the right (uses degrees NOT radians)
 	*	TurnLeft(float deg)
+	*		- Turns the actors direction to the left (uses degrees NOT radians)
 	* 
 	*/
 	class AdvancedActor: public Actor
