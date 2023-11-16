87062c2a75b983895129103a6cba686f0899c08b
Aidan Latno <aidlatno@outlook.com>
2023-11-15 19:28:47 -0800

    Implemented the InputManager class functionality (Only keyboard input)

--- LATNO_ENGINE/engine/definitions/InputManager.cpp
+++ LATNO_ENGINE/engine/definitions/InputManager.cpp
@@ -1,2 +1,11 @@
 #include "../declarations/InputManager.h"
 
+bool InputManager::KeyPressed(Key key)
+{
+	return glfwGetKey(window, key) == GLFW_PRESS;
+}
+
+bool InputManager::KeyReleased(Key key)
+{
+	return glfwGetKey(window, key) == GLFW_RELEASE;
+}
