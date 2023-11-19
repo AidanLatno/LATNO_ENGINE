c2fbc49f301c34d0fd86bfe1e5c3da27c7ba497a
AidanLatno <aidlatno@outlook.com>
2023-11-18 23:42:13 -0800

    Added documentation to both the InputManager and Button Classes

--- LATNO_ENGINE/Entities/declarations/Button.h
+++ LATNO_ENGINE/Entities/declarations/Button.h
@@ -3,6 +3,35 @@
 
 namespace Latno
 {
+	/*
+	* Class Name: Button
+	* Purpose: This class is meant to be inherited and overloaded, however you can use it with
+	*		   just the Hovering(), Pressed() and Released() methods. This class is meant to be
+	*		   a clickable actor with customizable behaviors for all kinds of events such as
+	*		   being clicked, hoving over it, and releasing the button.
+	* Parents: Actor -> Component -> Behavior
+	* 
+	* Update Override: Checks for events and calls the corresponding virtual methods
+	* 
+	* Virtual Methods:
+	*	void OnHover
+	*		- Called when the mouse hovers over the button
+	*	void OnPress
+	*		- Called repeatedly whilst left click is being held over the button
+	*	void OnRelease
+	*		- Called once after left click is released
+	*	void NotHover
+	*		- Called if the mouse is not hovering over the button
+	* 
+	* Methods:
+	*	bool Hovering
+	*		- returns true if the mouse is hovering over the button
+	*	bool Pressed()
+	*		- returns true if the left mouse button is being pressed whilst over the button
+	*	bool Released()
+	*		- returns true once after the button is released
+	*	
+	*/
 	class Button : public Actor
 	{
 		using Actor::Actor;
--- LATNO_ENGINE/engine/declarations/InputManager.h
+++ LATNO_ENGINE/engine/declarations/InputManager.h
@@ -6,12 +6,41 @@ namespace Latno
 	/*
 	* Class Name: InputManager
 	* Purpose: Querys all input events from keyboard presses to mouse clicks and position.
+	* Note: This uses the enum system named "Key", which can be found in the Definitions.h
+	*		file. The values of the enums are the same as the GLFW declarations, so they may
+	*		be used interchangably.
 	* 
 	* Variables:
 	*	oldState
 	*		- Keeps track of the previous state of all keys, using the key value as the index
 	*	window
 	*		- Keeps a reference to the window context to pull input from
+	* 
+	* Methods:
+	*	void SetWindow(GLFWwindow* _window)
+	*		- Sets the window context to get input from
+	*
+	*	bool KeyPressed(Key key)
+	*		- Will return true while a key is being pushed down
+	*	bool KeyReleased(Key key)
+	*		- Will return true if a key is not being pushed down
+	*	bool KeyDown(Key key)
+	*		- Will return true only once after a key is pressed until it is released
+	*	bool KeyUp(Key key)
+	*		- Will return true only once after a key is releassed until it is pressed again
+	*
+	*	Coords GetMousePos()
+	*		- Returns the coordinates of the mouse
+	*
+	*	bool MousePressed(Key key)
+	*		- Will return true while a mouse button is being pushed down
+	*	bool MouseReleased(Key key)
+	*		- Will return true if a mouse button is not being pushed down
+	*
+	*	bool MouseDown(Key key)
+	*		- Will return true only once after a mouse button is pressed until it is released
+	*	bool MouseUp(Key key)
+	*		- Will return true only once after a mouse button is releassed until it is pressed again
 	*/
 	class InputManager
 	{
